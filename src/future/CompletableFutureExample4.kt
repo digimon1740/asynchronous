216409a35e7c9aaf36aba4b63612635da7fb5600
tony <digimon.1740@gmail.com>
2020-09-20 00:36:02 +0900

    completableFuture 예제 수정

--- src/future/CompletableFutureExample4.kt
+++ src/future/CompletableFutureExample4.kt
@@ -5,16 +5,13 @@ import java.util.concurrent.*
 fun main(args: Array<String>) {
     val startTime = System.nanoTime()
     println("계산 시작")
-    (0 until 5).forEach {
-        val completableFuture = CompletableFuture.supplyAsync {
+    (0 until 5).map {
+        CompletableFuture.supplyAsync { // (1)
             println("current-thread-name : ${Thread.currentThread().name}")
             Calculator.sum(100, 200, delay = 2000)
         }
-        val result = completableFuture.join()
-        println(result)
-    }
+    }.map(CompletableFuture<Int>::join).forEach(::println)
     println("계산 종료")
     val elapsedTime = (System.nanoTime() - startTime) / 1_000_000
     println("수행시간 : $elapsedTime msecs")
 }
-
